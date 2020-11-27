f885a4bec0509ad69d6f4cb91df1dddfde25affe
utaiba <utaiba7@gmail.com>
2020-11-27 19:39:21 +0500

    pool data coming

--- src/Services/Pool.js
+++ src/Services/Pool.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
 export const getpools = (data) => {
-    return axios.request({
-        method: 'GET',
-        url: 'https://www.powerball.com/api/v1/numbers/powerball/recent?_format=json',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },
-        params: data
-    }).then(response => (
-        response.data
-    ))
+    return axios.all([
+        axios.get('https://lotteryhub.com/poc/lottery_numbers.json')
+    ])
+        // return axios.request({
+        //     method: 'GET',
+        //     url: 'https://lotteryhub.com/poc/lottery_numbers.json',
+        //     headers: {
+        //         'Access-Control-Allow-Origin': '*',
+        //         Accept: "application/json",
+        //         "Content-Type": "application/json"
+        //     }
+        // })
+        .then(response => (
+            response
+        ))
 }
