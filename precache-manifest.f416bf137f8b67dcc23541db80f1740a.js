15b41e80faa103184f4f1638618ead6ef1c85170
“utaiba” <utaiba7@gmail.com>
2020-12-22 08:37:29 -0500

    Updates

---
+++ precache-manifest.f416bf137f8b67dcc23541db80f1740a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0775426c5dd2b9a523d3d614b19cbbf3",
+    "url": "/DreamLegacy-react/index.html"
+  },
+  {
+    "revision": "481eabd68041318cd524",
+    "url": "/DreamLegacy-react/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "1623d271be6cbd0c47f2",
+    "url": "/DreamLegacy-react/static/js/2.1ae59232.chunk.js"
+  },
+  {
+    "revision": "ba0a3b7f935a8119af5d30fb57033688",
+    "url": "/DreamLegacy-react/static/js/2.1ae59232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481eabd68041318cd524",
+    "url": "/DreamLegacy-react/static/js/main.4c0c1145.chunk.js"
+  },
+  {
+    "revision": "af305a3e41ea4512245d",
+    "url": "/DreamLegacy-react/static/js/runtime-main.4c4a4e6b.js"
+  },
+  {
+    "revision": "2674f5a995caf37ea88c0f8a3337b6a9",
+    "url": "/DreamLegacy-react/static/media/US-Map.2674f5a9.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/DreamLegacy-react/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "a6b0ac32ba1f5d3fd165c55d92bcef0d",
+    "url": "/DreamLegacy-react/static/media/bg.a6b0ac32.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/DreamLegacy-react/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "4b7346bfcd76aecfefb9b45dc4d622e9",
+    "url": "/DreamLegacy-react/static/media/bg7.4b7346bf.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/DreamLegacy-react/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/DreamLegacy-react/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/DreamLegacy-react/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/DreamLegacy-react/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/DreamLegacy-react/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "2674f5a995caf37ea88c0f8a3337b6a9",
+    "url": "/DreamLegacy-react/static/media/landing-bg.2674f5a9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/DreamLegacy-react/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/DreamLegacy-react/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "cde4cee25ee71e64f9ba7ebffc15407b",
+    "url": "/DreamLegacy-react/static/media/profile-bg.cde4cee2.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/DreamLegacy-react/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/DreamLegacy-react/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/DreamLegacy-react/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/DreamLegacy-react/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/DreamLegacy-react/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "54c31458d52090a4c8740d123bdeaa47",
+    "url": "/DreamLegacy-react/static/media/users.54c31458.jpg"
+  }
+]);
